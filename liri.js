c023c7649d54398cd51230550cf14598ca7e93eb
mpleier <mpleier@gmail.com>
2017-08-17 13:55:30 -0700

    enhanced error reporting

--- liri.js
+++ liri.js
@@ -1,17 +1,13 @@
 var keys = require('./keys');
-
 var Twitter = require('twitter');
 const fs = require('fs');
 var endOfLine = require('os').EOL;
-
 function log(data) {
     console.log(data);
     fs.appendFile('log.txt', data + endOfLine, function(err) {
         if (err) throw err;
     });
 }
-
-
 function tweetTweet() {
     var client = new Twitter({
         consumer_key: keys.twitterKeys.consumer_key,
@@ -19,30 +15,21 @@ function tweetTweet() {
         access_token_key: keys.twitterKeys.access_token_key,
         access_token_secret: keys.twitterKeys.access_token_secret
     });
-
-
     var params = {
         screen_name: 'ThereAintNoJust'
     };
     var o = "";
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-
             for (i = 0; i < 20; i++) {
                 o += tweets[i].text + endOfLine;
                 o += tweets[i].created_at + endOfLine;
                 o += endOfLine;
             }
-
             log(o);
-
-
         }
     });
 }
-
-
-
 function spotThis(song) {
     var Spotify = require('node-spotify-api');
     var spotify = new Spotify({
@@ -62,7 +49,6 @@ function spotThis(song) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
         log(data.tracks.items[0].artists[0].name);
         log(data.tracks.items[0].name);
         log(data.tracks.items[0].external_urls.spotify);
@@ -70,30 +56,23 @@ function spotThis(song) {
         log(endOfLine);
     });
 }
-
-
 function watchThis() {
-
     var a;
     if (process.argv.slice(3) != "") {
         a = process.argv.slice(3);
     } else {
         a = 'Mr. Nobody.';
     }
-
     var request = require('request');
     var rt = "Rotten Tomatoes Rating of the movie not found.";
     request.get({
         url: 'http://www.omdbapi.com/?apikey=40e9cece&t=' + a,
         json: true
     }, function(e, r, user) {
-
         if (typeof variable !== 'undefined') {
             rt = user.Ratings[1].Value;
         }
         if (typeof user.Title !== 'undefined') {
-
-
             log(user.Title);
             log(user.Year);
             log(user.imdbRating);
@@ -106,61 +85,44 @@ function watchThis() {
         } else {
             log("Movie not found.");
             log(endOfLine);
-
         }
     });
-
 }
 
-
+function errors() {
+  var e = ["PEBCAK", "EBKAC", "POBCAK", "One Delta Ten Tango", "Eye Dee Ten Tango", "PICNIC", "IBM error", "layer 8 issue", "wetware bug", "loose nut behind the keyboard", "short between the headphones","defective PTT button actuator", "OHT", "Error 40", "ICC", "Cas-18", "carbon based error"];
+  return e[Math.floor(Math.random() * 17)];
+}
 
 switch (process.argv[2]) {
     case "my-tweets":
         tweetTweet();
         break;
     case "spotify-this-song":
-
         spotThis();
-
-
         break;
     case "movie-this":
-
         watchThis();
-
         break;
     case "do-what-it-says":
-
         var text = fs.readFileSync('random.txt', 'utf8');
         var args = text.split(",");
-
-
         switch (args[0]) {
             case "my-tweets":
                 tweetTweet();
                 break;
             case "spotify-this-song":
-
                 spotThis(args[1]);
-
-
                 break;
             case "movie-this":
-
                 watchThis(args[1]);
-
                 break;
-
             default:
-                log("PEBCAK");
+                log(errors());
                 log(endOfLine);
         }
-
-
-
-
         break;
     default:
-        log("PEBCAK");
+        log(errors());
         log(endOfLine);
 }
